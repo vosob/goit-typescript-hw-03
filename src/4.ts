3f5a9f2f90137475447aceaa10ec35fdf48b09b1
vosob <osobskyi.vlad@gmail.com>
2024-01-30 04:31:11 +0200

    f

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,52 @@
-const key = new Key();
+class Key {
+  private signature: number = Math.random();
 
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+  constructor(protected key: Key) {}
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Door is open");
+    } else {
+      console.log("Door is not open");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door is open");
+    } else {
+      console.log("Door is closed");
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
